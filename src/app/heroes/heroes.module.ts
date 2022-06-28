35713dfd3b5c47dc9bb2655cd30ec9c21e7767a8
V473R10 <itsme@facundovalerio.com>
2022-06-27 22:52:31 -0300

    Material Installed

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 import { AddComponent } from './pages/add/add.component';
-import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
+import { SearchComponent } from './pages/search/search.component';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -12,10 +18,14 @@ import { ListComponent } from './pages/list/list.component';
     AddComponent,
     SearchComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    HeroeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexLayoutModule,
+    HeroesRoutingModule,
+    MaterialModule
   ]
 })
 export class HeroesModule { }
